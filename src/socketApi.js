b853abd9a2fb3564347401d9e3f3183d2d7c4998
KursatKeremCevlik <kur.ker.cev@gmail.com>
2020-08-31 15:37:18 +0300

    packages

---
+++ src/socketApi.js
@@ -0,0 +1,15 @@
+const socketio = require('socket.io');
+const io = socketio();
+
+const socketApi = {  };
+socketApi.io = io;
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('disconnect', () => {
+      console.log('a user disconnected');
+  });
+});
+
+module.exports = socketApi;
