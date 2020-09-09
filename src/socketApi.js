e6b6f8b3fcc6a10befecd4517814879f47acd5bf
KursatKeremCevlik <kur.ker.cev@gmail.com>
2020-09-09 23:49:04 +0300

    adminPage edit

--- src/socketApi.js
+++ src/socketApi.js
@@ -5,11 +5,7 @@ const socketApi = {};
 socketApi.io = io;
 
 io.on('connection', (socket) => {
-  // console.log('a user connected');
 
-  socket.on('disconnect', () => {
-    // console.log('a user disconnected');
-  });
 });
 
 module.exports = socketApi;
--- views/admin.js
+++ views/admin.js
@@ -1,8 +1,31 @@
 $(() => {
     const socket = io.connect('http://localhost:3000');
 
-    $('.link').on('click', () => {
-        $('.AdminHomePage').hide();
-        $('.AdminBlogPage').show();
+    let BlogTitle;
+    let BlogContent;
+    let BlogDate;
+
+    $('.writeBlogButton').on('click', () => {
+        $('.firstPage').hide();
+        $('.secondPage').show();
+    });
+
+    $('.ContentButon').on('click', () => {
+        const BlogTitleValue = document.getElementById('title');
+        const BlogDateValue = document.getElementById('date');
+        BlogTitle = BlogTitleValue.value;
+        BlogDate = BlogDateValue.value;
+        $('.BlogFormHome').hide();
+        $('.ContentFormHome').show();
+    });
+
+    $('.PreviewButon').on('click', () => {
+        const BlogContentValue = document.getElementById('content');
+        BlogContent = BlogContentValue.value;
+        $('.ContentFormHome').hide();
+        $('.PreviewPage').show();
+        $('.PreviewBlogTitle').html(BlogTitle);
+        $('.PreviewBlogContent').html(`<a class="space">....</a>${BlogContent}`);
+        $('.PreviewBlogDate').html(BlogDate);
     });
 });
