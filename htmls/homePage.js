9d5712b857155cc83015055a4d863b2dbed84122
KursatKeremCevlik <kur.ker.cev@gmail.com>
2020-09-12 16:29:55 +0300

    homePage edit

--- htmls/homePage.js
+++ htmls/homePage.js
@@ -5,11 +5,30 @@ $(() => {
 
     socket.on('BLOGS', veri => {
         $('.Blogs').append(`
-            <div class="BlogHome">
+            <div class="BlogHome" id="BlogHome">
                 <div class="BlogTitle">${veri.title}</div>
-                <div class="BlogContent"><a class="space">....</a>${veri.content}</div>
+                <div class="BlogContent"><a class="space" id="space">....</a>${veri.content}</div>
                 <div class="BlogDate">${veri.date}</div>
             </div>
         `);
+    });
+    
+    $('.darkModeButon').on('click', () => {
+        document.body.style.backgroundColor = "black";
+        document.getElementById("BlogHome").style.color = "white";
+        document.getElementById("space").style.color = "black";
+        document.getElementById("Medium").style.color = "white";
+        document.getElementById("Name").style.color = "white";
+        document.getElementById("buton1").style.border = '1px solid black';
+        document.getElementById("buton2").style.border = '1px solid black';
+    });
+    $('.lightModeButon').on('click', () => {
+        document.body.style.backgroundColor = "white";
+        document.getElementById("BlogHome").style.color = "black";
+        document.getElementById("space").style.color = "white";
+        document.getElementById("Medium").style.color = "black";
+        document.getElementById("Name").style.color = "black";
+        document.getElementById("buton1").style.border = '1px solid white';
+        document.getElementById("buton2").style.border = '1px solid white';
     });
 });
