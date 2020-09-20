0568c5c966e5abf7dcef10fb2ac6a20ccc787a04
KursatKeremCevlik <kur.ker.cev@gmail.com>
2020-09-20 13:19:59 +0300

    code lint

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const app = express();
+
 const URL = 'mongodb+srv://ownerUser:12345@cluster0.zqmnm.mongodb.net/blog';
 // const URL = 'mongodb://localhost/myBlog';
 const mongoDB = require('./helper/db')(URL);
@@ -11,24 +12,22 @@ const mongoDB = require('./helper/db')(URL);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-
-
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/sheets/htmls/homePage.html');
 });
 app.get('/admin', (req, res) => {
     res.sendFile(__dirname + '/sheets/htmls/adminPage.html');
 });
-app.use('/css/homePage', express.static(path.join(__dirname, '/sheets/css/homePage.css')));
-app.use('/js/homePage', express.static(path.join(__dirname, '/sheets/js/homePage.js')));
 
+app.use('/css/homePage', express.static(path.join(__dirname, '/sheets/css/homePage.css')));
 app.use('/css/adminPage', express.static(path.join(__dirname, '/sheets/css/adminPage.css')));
+app.use('/js/homePage', express.static(path.join(__dirname, '/sheets/js/homePage.js')));
 app.use('/js/adminPage', express.static(path.join(__dirname, '/sheets/js/adminPage.js')));
 
 
 
+const routes = require('./operations/routes')(app, express, path);
 const expressOprt = require('./operations/expressOprt')(app, express, logger, cookieParser, path);
-
 const Errors = require('./operations/errors')(app, createError);
 
 module.exports = app;
